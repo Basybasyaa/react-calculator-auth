031a786bfd226b7f1bbebb30b64cfbe5ac359cb1
Basybasyaa <118032542+Basybasyaa@users.noreply.github.com>
2026-01-09 11:03:22 +0700

    add new file: src/utils/calculate.js

--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
-import { add } from "../utils/calculate";
+import { add, subtract, multiply } from "../utils/calculate";
 
 function Calculator() {
-  const [a, setA] = useState(0);
-  const [b, setB] = useState(0);
+  const [a, setA] = useState("");
+  const [b, setB] = useState("");
 
   return (
     <div>
@@ -12,17 +12,26 @@ function Calculator() {
       <input
         type="number"
         value={a}
-        onChange={(e) => setA(Number(e.target.value))}
+        onChange={(e) => setA(e.target.value)}
       />
       <input
         type="number"
         value={b}
-        onChange={(e) => setB(Number(e.target.value))}
+        onChange={(e) => setB(e.target.value)}
       />
 
-      <button onClick={() => alert(`Hasil: ${add(a, b)}`)}>
+      <button onClick={() => alert(`Hasil: ${add(Number(a), Number(b))}`)}>
         Tambah
       </button>
+      
+      <button onClick={() => alert(`Hasil: ${subtract(Number(a), Number(b))}`)}>
+        Kurang
+      </button>
+
+      <button onClick={() => alert(`Hasil: ${multiply(Number(a), Number(b))}`)}>
+        Kali
+      </button>
+
     </div>
   );
 }
